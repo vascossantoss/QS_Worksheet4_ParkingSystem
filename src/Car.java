7812ed479dc3600c9caff1d90b65c1c11b31d32e
Vasco Santos <silvavascosantos@gmail.com>
2024-10-03 10:26:30 +0100

    Updated Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,9 +1,56 @@
 /**
  * @author vss
- * @version 1.0
+ * @version 1.1
  *
  * Class that represents car objects
  */
 public class Car {
+    private String brand;
+    private String model;
+    private String licencePlate;
 
+    /**
+     * Constructor
+     * @param brand
+     * @param model
+     * @param licencePlate
+     */
+    public Car(String brand, String model, String licencePlate) {
+        this.brand = brand;
+        this.model = model;
+        this.licencePlate = licencePlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", licencePlate='" + licencePlate + '\'' +
+                '}';
+    }
 }
